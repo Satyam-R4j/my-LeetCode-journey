ed0ff0a60c22e55fb15dc3cba6f3d27869d41a55
Satyam-R4j <satyamraj418@gmail.com>
2025-03-09 04:02:33 +0530

    Problem-204

---
+++ Medium Problem's/p-204. Count Primes/Solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n)
+{
+    let isPrime = new Array(n + 1)
+    isPrime.fill(true)
+    let count = 0
+
+    for (let i = 2; i < n; i++)
+    {
+        if (isPrime[i])
+        {
+            count++
+            for (let j = i * 2; j < n; j = j + i)
+            {
+                isPrime[j] = false
+            }
+        }
+    }
+
+    return count
+
+};
+
+let n = 13
+console.log(countPrimes(n))
