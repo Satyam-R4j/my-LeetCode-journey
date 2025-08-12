17344dfdc971f9505cc7d9e31e99879ba5427a75
Satyam-R4j <satyamraj418@gmail.com>
2025-08-12 23:02:39 +0530

    Problem-42 Optimal Solution

--- Hard Problem's/p-42. Trapping Rain Water/Solution.js
+++ Hard Problem's/p-42. Trapping Rain Water/Solution.js
@@ -5,30 +5,58 @@
 
 //Satyam-R4j-LeetCode Problem-42 (Hard)
 //Solution in JavaScript
+// var trap = function (height)
+// {
+//     let left = new Array(height.length)
+//     let right = new Array(height.length)
+//     let ans = null
+//     let maxLeft = height[0], maxRight = height[height.length - 1]
+//     left[0] = maxLeft, right[right.length - 1] = maxRight
+
+//     for (let i = 1; i < height.length; i++)
+//     {
+//         maxLeft = Math.max(maxLeft, height[i])
+//         left[i] = maxLeft
+//     }
+//     for (let i = height.length - 2; i >= 0; i--)
+//     {
+//         maxRight = Math.max(maxRight, height[i])
+//         right[i] = maxRight
+//     }
+//     for (let i = 0; i < height.length; i++)
+//     {
+//         ans += Math.min(left[i], right[i]) - height[i]
+//     }
+//     return ans
+// };
+
 var trap = function (height)
 {
-    let left = new Array(height.length)
-    let right = new Array(height.length)
-    let ans = null
-    let maxLeft = height[0], maxRight = height[height.length - 1]
-    left[0] = maxLeft, right[right.length - 1] = maxRight
+    let ans = 0 
+    let n = height.length
+    let l = 0 , r = n - 1
+    let maxL = 0, maxR =0
 
-    for (let i = 1; i < height.length; i++)
+    while(l<r)
     {
-        maxLeft = Math.max(maxLeft, height[i])
-        left[i] = maxLeft
-    }
-    for (let i = height.length - 2; i >= 0; i--)
-    {
-        maxRight = Math.max(maxRight, height[i])
-        right[i] = maxRight
-    }
-    for (let i = 0; i < height.length; i++)
-    {
-        ans += Math.min(left[i], right[i]) - height[i]
+        maxL = Math.max(maxL,height[l])
+        maxR = Math.max(maxR, height[r])
+
+        if(maxL < maxR)
+        {
+            ans += maxL - height[l]
+            l++
+        }
+        else{
+            ans += maxR -  height[r]
+            r--
+        }
     }
     return ans
-};
+}
+
+
+
 
 let height = [4, 2, 0, 3, 2, 5]
 console.log(trap(height));
