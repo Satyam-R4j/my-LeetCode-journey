271cae8317c799b97ee3b979ab7eb00c893d9c7d
Satyam-R4j <satyamraj418@gmail.com>
2025-09-04 20:50:49 +0530

    Problem-94 another Solution

--- Easy Problem's/p-94. Binary Tree Inorder Traversal/Solution.js
+++ Easy Problem's/p-94. Binary Tree Inorder Traversal/Solution.js
@@ -19,16 +19,43 @@ function TreeNode(val, left, right) {
 //Satyam-R4j-LeetCode Problem-94 (easy)
 //Solution in JavaScript
 var inorderTraversal = function (root) {
-
-    let result = new Array()
+    let result = new Array();
     function helper(node, result) {
-        if(!node) return 
-        helper(node.left,result);
+        if (!node) return;
+        helper(node.left, result);
         result.push(node.val);
-        helper(node.right,result);
+        helper(node.right, result);
     }
     helper(root, result);
     return result;
+};
+
+//Satyam-R4j-LeetCode Problem-94 (easy)
+//Solution in JavaScript
+var inorderTraversal = function (root) {
+    let ans = [];
+    let curr = root;
+
+    while (curr !== null) {
+        if (curr.left === null) {
+            ans.push(curr.val);
+            curr = curr.right;
+        } else {
+            let IP = curr.left;
+            while (IP.right !== null && IP.right !== curr) {
+                IP = IP.right;
+            }
+            if (IP.right === null) {
+                IP.right = curr;
+                curr = curr.left;
+            } else {
+                IP.right = null;
+                ans.push(curr.val);
+                curr = curr.right;
+            }
+        }
+    }
+    return ans;
 };
 
 let root = TreeNode([1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9]);
