252071918d61019f95fa9642b030fa3fd29df083
Satyam-R4j <satyamraj418@gmail.com>
2025-12-12 00:16:16 +0530

    Problem-70 using recursion Solution.js

--- Easy Problem's/p-70. Climbing Stairs/Solution.js
+++ Easy Problem's/p-70. Climbing Stairs/Solution.js
@@ -2,20 +2,33 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
-    if (n <= 3)  return n
-    let pre1 = 3
-    let pre2 = 2
-    let curr = 0
+// var climbStairs = function(n) {
+//     if (n <= 3)  return n
+//     let pre1 = 3
+//     let pre2 = 2
+//     let curr = 0
 
-    for (let i = 3; i < n; i++) {
-        curr = pre1 + pre2
-        pre2 = pre1
-        pre1 = curr
-    }
-    return curr
+//     for (let i = 3; i < n; i++) {
+//         curr = pre1 + pre2
+//         pre2 = pre1
+//         pre1 = curr
+//     }
+//     return curr
 
+// };
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+//Satyam-R4j-LeetCode Problem-70 (easy)
+//Solution in JavaScript
+var climbStairs = function (n, memo = {}) {
+    if (n <= 3) return n;
+    if (memo[n]) return memo[n];
+    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+    return memo[n];
 };
 
-let n = 5
-console.log(climbStairs(n))
+let n = 4;
+console.log(climbStairs(n));
