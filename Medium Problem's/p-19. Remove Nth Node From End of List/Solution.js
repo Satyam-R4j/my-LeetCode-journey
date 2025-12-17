aea5e85de031c38a6624e057feb8f27ad36f46ef
Satyam-R4j <satyamraj418@gmail.com>
2025-12-17 21:51:44 +0530

    Problem-19 Solution

--- Medium Problem's/p-19. Remove Nth Node From End of List/Solution.js
+++ Medium Problem's/p-19. Remove Nth Node From End of List/Solution.js
@@ -1,64 +1,20 @@
-function ListNode(val, next = null)
-{
-    this.val = val;
-    this.next = next;
-}
-
 //Satyam-R4j-LeetCode Problem-19 (medium)
 //Solution in JavaScript
-var removeNthFromEnd = function (head, n)
-{
-
-    if (head === null) return null
-    let len = 0
-    let curr = head
-
-    while (curr !== null)
-    {
-        len++
-        curr = curr.next
-    }
-
-    let nth = len - n
-    if (nth === 0) return head.next
-    let i = 1
-    curr = head
-    while (i < nth)
-    {
-        curr = curr.next
-        i++
-    }
-    curr.next = curr.next ? curr.next.next : null
-    return head
-};
-
-
-function arrayToLinkedList(arr)
-{
-    let dummy = new ListNode(0);
-    let current = dummy;
-    for (let val of arr)
-    {
-        current.next = new ListNode(val);
+var removeNthFromEnd = function (head, n) {
+    if (!head) return null;
+    let length = 0;
+    let current = head;
+    while (current) {
+        length++;
         current = current.next;
     }
-    return dummy.next;
-}
-
-function linkedListToArray(head)
-{
-    let result = [];
-    while (head !== null)
-    {
-        result.push(head.val);
-        head = head.next;
+    if (length === n) {
+        return head.next;
     }
-    return result;
-}
-
-
-let head = arrayToLinkedList([1, 2]);
-let n = 1;
-
-let updatedHead = removeNthFromEnd(head, n);
-console.log(linkedListToArray(updatedHead));
+    current = head;
+    for (let i = 1; i < length - n; i++) {
+        current = current.next;
+    }
+    current.next = current.next.next;
+    return head;
+};
