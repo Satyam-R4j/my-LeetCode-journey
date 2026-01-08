92f5a2b14fb778e9a9af08de5069e703df7fe8ed
Satyam-R4j <satyamraj418@gmail.com>
2026-01-08 15:56:28 +0530

    Problem-56 Solution

---
+++ Medium Problem's/p-56. Merge Intervals/Solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+//Satyam-R4j-LeetCode Problem-56 (medium)
+//Solution in JavaScript
+var merge = function (intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    console.log(intervals);
+
+    let ans = [];
+    let currInterval = intervals[0];
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] <= currInterval[1]) {
+            currInterval[1] = Math.max(currInterval[1], intervals[i][1]);
+        } else {
+            ans.push(currInterval);
+            currInterval = intervals[i];
+        }
+    }
+
+    ans.push(currInterval);
+    return ans;
+};
+
+let intervals = [
+    [2, 6],
+    [1, 3],
+    [8, 10],
+    [15, 18],
+];
+console.log(merge(intervals));
