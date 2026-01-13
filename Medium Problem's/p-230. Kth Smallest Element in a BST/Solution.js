8903dfc01086c9b188f1080122568c111009642f
Satyam-R4j <satyamraj418@gmail.com>
2026-01-13 14:45:37 +0530

    Problem-230 new Solution

--- Medium Problem's/p-230. Kth Smallest Element in a BST/Solution.js
+++ Medium Problem's/p-230. Kth Smallest Element in a BST/Solution.js
@@ -14,20 +14,17 @@
 //Satyam-R4j-LeetCode Problem-230 (medium)
 //Solution in JavaScript
 var kthSmallest = function (root, k) {
-    preOrder = 0;
-    function helper(root, k) {
+    let count = 0;
+    function helper(root) {
         if (root === null) return -1;
         if (root.left !== null) {
-            let leftAns = helper(root.left, k);
+            let leftAns = helper(root.left);
             if (leftAns !== -1) return leftAns;
         }
-        if (preOrder + 1 === k) return root.val;
-        preOrder = preOrder + 1;
-        if (root.right !== null) {
-            let rightAns = helper(root.right, k);
-            if (rightAns !== -1) return rightAns;
-        }
-        return -1;
+        count++
+        if (count === k) return root.val
+
+        return helper(root.right)
     }
     return helper(root, k);
 };
