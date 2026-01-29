2af36e4d3ae4d0c30a120c917e0100e37ca91206
Satyam-R4j <satyamraj418@gmail.com>
2026-01-29 16:03:39 +0530

    Problem-145 Solution.py

---
+++ Easy Problem's/p-145. Binary Tree Postorder Traversal/Solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]: # type: ignore
+        def helper(node):
+            if not node:
+                return
+            helper(node.left)
+            helper(node.right)
+            ans.append(node.val)
+        
+        ans = []
+        helper(root)
+        return ans
