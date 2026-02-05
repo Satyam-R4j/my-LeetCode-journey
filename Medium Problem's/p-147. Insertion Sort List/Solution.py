3539413b9b9a8c69deec602a1db41b217c44d0d2
Satyam-R4j <satyamraj418@gmail.com>
2026-02-05 13:04:09 +0530

    Problem-147 Solution.py

---
+++ Medium Problem's/p-147. Insertion Sort List/Solution.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]: # type: ignore
+
+        dumy = ListNode(0)
+        curr = head
+        while curr:
+            next_node = curr.next
+            prev = dumy
+
+            while prev.next and prev.next.val < curr.val:
+                prev= prev.next
+
+            curr.next = prev.next
+            prev.next = curr
+
+            curr = next_node
+        
+        return dumy.next
+
+
+                
+
+            
+
+
+
+        
+
+
+
+
+# Input: head = [4,2,1,3]
+# Output: [1,2,3,4]
